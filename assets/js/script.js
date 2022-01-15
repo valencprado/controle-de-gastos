96474623469eb702adcaae8c6ada8fcbec3e3895
valencprado <valenpschool@gmail.com>
2022-01-15 20:35:04 -0300

    arquivo js (incompleto)

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+// capturar valores
+// armazenar valores de alguma forma
+//  desenvolver funções que realizem os cálculos
+// atualizar interface
+
+// capturar valores
+// caoturar campos de entrada de valores
+// capturar valores dos campos
+const campoEntradaOrcamento = document.querySelector('.FormAddOrcamento input')
+const buttonOrcamento = document.querySelector('.FormAddOrcamento button')
